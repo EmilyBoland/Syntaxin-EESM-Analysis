c812c48b7262f0b4b73b7af02863efbce138dcf4
EmilyBoland <68649775+EmilyBoland@users.noreply.github.com>
2021-02-22 02:28:20 -0500

    Update work_analysis_functions.py

--- work_analysis_functions.py
+++ work_analysis_functions.py
@@ -8,10 +8,10 @@ def calc_work(alldata, mem, run):
   output: work for all three distributions of one run of one member
   """
 
-  work = 0      
-  for dimension in alldata['converged run data'][mem][run]:
-
-    Rs = alldata['converged run data'][mem][run][dimension]['R']
+  work = 0 
+  dimensions = ['052_210', '105_216', '196_228']     
+  for dim in dimensions:
+    Rs = alldata['converged run data'][mem][run][dim]['R']
     distances = []
     for i in range(0,len(Rs)-1):
       lowerR = Rs[i]
@@ -19,8 +19,8 @@ def calc_work(alldata, mem, run):
       distance = upperR-lowerR
       distances.append(distance)
         
-    alpha = alldata['converged run data'][mem][run][dimension]['alpha']
-    target = alldata['converged run data'][mem][run][dimension]['target']
+    alpha = alldata['converged run data'][mem][run][dim]['alpha']
+    target = alldata['converged run data'][mem][run][dim]['target']
     force = alpha/target 
     for distance in distances:
       work += abs(distance*force)
@@ -139,7 +139,7 @@ def twoD_sum(data):
 
 
 
-def make_map(plot_dict):
+def make_map(plot_dict, colorchoice = "hot"):
   """
   plots the data given in a 2-dimensional array
   inputs: 
@@ -160,18 +160,33 @@ def make_map(plot_dict):
   for key in plot_dict:
     xlabel = key[9:]
     ylabel = key[0:7]
-    fig.add_subplot(3,3,i)
-    plt.imshow(plot_dict[key], 
+    if colorchoice == 'hot':
+      fig.add_subplot(3,3,i)
+      plt.imshow(plot_dict[key], 
         interpolation=None, 
-        origin='lower', cmap='hot')
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.colorbar()
-    plt.title("{} v. {}".format(ylabel, xlabel))
-    plt.xticks(np.arange(16), ('','1.0', '','2.0', '','3.0','', '4.0','', '5.0','','6.0','','7.0','','8.0'))
-    plt.yticks(np.arange(16), ('','1.0', '','2.0', '','3.0','', '4.0','', '5.0','','6.0','','7.0','','8.0'))
-    i += 1
-
+        origin='lower', cmap= colorchoice)
+      plt.xlabel(xlabel)
+      plt.ylabel(ylabel)
+      plt.colorbar()
+      plt.title("{} v. {}".format(ylabel, xlabel))
+      plt.xticks(np.arange(16), ('','1.0', '','2.0', '','3.0','', '4.0','', '5.0','','6.0','','7.0','','8.0'))
+      plt.yticks(np.arange(16), ('','1.0', '','2.0', '','3.0','', '4.0','', '5.0','','6.0','','7.0','','8.0'))
+    else:
+      bound = max(max(abs(x)) for x in plot_dict[key])
+      xlabel = key[9:]
+      ylabel = key[0:7]
+      fig.add_subplot(3,3,i)
+      plt.imshow(plot_dict[key], 
+        interpolation=None, 
+        origin='lower', cmap= colorchoice, 
+        vmin = -bound, vmax = bound)
+      plt.xlabel(xlabel)
+      plt.ylabel(ylabel)
+      plt.colorbar()
+      plt.title("{} v. {}".format(ylabel, xlabel))
+      plt.xticks(np.arange(16), ('','1.0', '','2.0', '','3.0','', '4.0','', '5.0','','6.0','','7.0','','8.0'))
+      plt.yticks(np.arange(16), ('','1.0', '','2.0', '','3.0','', '4.0','', '5.0','','6.0','','7.0','','8.0'))
+    i += 1 
   return True
 
 
@@ -219,31 +234,53 @@ def JScalculator(pi, pf):
 
 
 
-def plot_js_along_runs(by_run_dict, final_dict, dat_type):
+def plot_js_along_runs(by_run_dict, final_dict, final_dict_1D, dat_type):
   """
   creates a plot of JS divergence between final distribution v. simulation data along runs (cumulative)
   inputs: 
     by_run_dict = the data by run (cumulative) to calculate JS divergence with
     final_dict = the final data to calculate JS divergence with 
+    final_dict_1D = a 3 key:value pair dictionary containing 3 lists with the final data summed along all three axes
     dat_type = str describing source of data in final_dict. Either "Experimental" or "Simulation"
   output: 
-    a plot containing 3 curves: one for each distribution
+    a plot containing 4 curves: one for each distribution and an overall
   """
 
   import matplotlib.pyplot as plt
+  import numpy as np
+
+  by_run_1D = {'052_210':{}, '105_216': {}, '196_228':{}}
+  for run in by_run_dict:
+    by_run_1D['052_210'][run] = np.sum(by_run_dict[run], axis =(1,2))
+    by_run_1D['105_216'][run] = np.sum(by_run_dict[run], axis =(0,2))
+    by_run_1D['196_228'][run] = np.sum(by_run_dict[run], axis =(0,1))
+
+  total_js_divs = []
+  by_run_lst_dict = {}
+  for run in by_run_dict:
+    final_lst = []
+    by_run_lst = []
+    by_run_data = by_run_dict[run]
+    for d1 in range(17):
+      for d2 in range(17):
+        for d3 in range(17):
+          final_lst.append(final_dict[d1,d2,d3])
+          by_run_lst.append(by_run_data[d1,d2,d3])
+    total_js_divs.append(JScalculator(final_lst, by_run_lst))
 
   jsdivs_by_run = {}
-  for distr in by_run_dict:
+  for distr in by_run_1D:
     jsdivs_by_run[distr] = []
-    for run in by_run_dict[distr]:
-      jsdivs_by_run[distr].append(JScalculator(by_run_dict[distr][run], final_dict[distr]))
+    for run in by_run_1D[distr]:
+      jsdivs_by_run[distr].append(JScalculator(by_run_1D[distr][run], final_dict_1D[distr]))
 
-  x = [1,2,3,4,5,6,7,8,9,10]
-  plt.plot(x,jsdivs_by_run['052_210'],'c',lw = 3)
-  plt.plot(x,jsdivs_by_run['105_216'],'purple', lw = 3)
-  plt.plot(x,jsdivs_by_run['196_228'],'orange' , lw = 3)
+  runs = [1,2,3,4,5,6,7,8,9,10]
+  plt.plot(runs,jsdivs_by_run['052_210'],':', c = "purple", lw = 3)
+  plt.plot(runs,jsdivs_by_run['105_216'],'--c', lw = 3)
+  plt.plot(runs,jsdivs_by_run['196_228'],'-.', c = 'orange', lw = 3)
+  plt.plot(runs, total_js_divs, '-r', lw = 3)
   plt.xlabel("Number of Runs (Cumulative)")
   plt.ylabel("JS-Divergence")
   plt.title("JS-Divergence between final {} Joint Distribution \n and Simulation Distribution along Number of Runs (Cumulative)".format(dat_type))
-  plt.legend(['052_210', '105_216', '196_228'])
+  plt.legend(['052_210', '105_216', '196_228', 'total'])
 
